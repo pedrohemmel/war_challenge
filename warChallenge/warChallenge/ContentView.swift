2258454f6e65f0cebca02599de14f73adc73b5c2
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2022-08-22 17:31:08 -0300

    Creating the project and puting some aux files into the project

---
+++ warChallenge/warChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  warChallenge
+//
+//  Created by Pedro Henrique Dias Hemmel de Oliveira Souza on 22/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ warChallenge/warChallenge/warChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  warChallengeApp.swift
+//  warChallenge
+//
+//  Created by Pedro Henrique Dias Hemmel de Oliveira Souza on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct warChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
